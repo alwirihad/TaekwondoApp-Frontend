1596e3ccc78eea265d41f14fd50ceb38e284ea02
Alwi Rihad <alwirihad8@gmail.com>
2025-09-26 22:15:23 +0700

    Update web Taekwondo: - Add search feature in bagan.html (by name & no_partai) - Add QR code for peserta nametag - Fix sorting of no_partai - Fix API fetch token issue in peserta page - Fix peserta table display - Update HTML/CSS/JS for responsiveness - Clean up unused files and optimize structure

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export const BASE_URL = "https://taekwondo-app-jet.vercel.app";
+=======
+export const BASE_URL = "https://taekwondo-app-jet.vercel.app/"; //PRODUCTION
+>>>>>>> a953a22 (Update web Taekwondo:)
