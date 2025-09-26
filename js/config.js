c66d9f472cbda1c89baf6a0a9a0637923ca85b97
Alwi Rihad <alwirihad8@gmail.com>
2025-09-26 22:21:16 +0700

    Initial commit backend / Update backend API

--- js/config.js
+++ js/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const BASE_URL = "https://taekwondo-app-jet.vercel.app";
-=======
 export const BASE_URL = "https://taekwondo-app-jet.vercel.app/"; //PRODUCTION
->>>>>>> a953a22 (Update web Taekwondo:)
